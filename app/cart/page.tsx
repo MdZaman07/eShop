a76fed35ec4fe295ddfb20cea1aea78fd4940d6a
Pulok <pulok@property.credit>
2024-02-24 12:30:47 +1100

    product details page

---
+++ app/cart/page.tsx
@@ -0,0 +1,5 @@
+const Cart = () => {
+  return <div>Cart Pages</div>;
+};
+
+export default Cart;
--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,13 +1,24 @@
 "use client";
 
+import { formatPrice } from "@/utils/formatPrice";
+import { truncateText } from "@/utils/truncateText";
+import { Rating } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
   data: any;
 }
 const ProductCard = ({ data }: ProductCardProps) => {
+  const router = useRouter();
+  const productRating =
+    data.reviews.reduce((accum: number, item: any) => item.rating + accum, 0) /
+    data.reviews.length;
   return (
-    <div className="col-span-1 cursor-pointer border-[1,2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm ">
+    <div
+      onClick={() => router.push(`/product/${data.id}`)}
+      className="col-span-1 cursor-pointer border-[1,2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm "
+    >
       <div
         className="
       flex
@@ -24,10 +35,12 @@ const ProductCard = ({ data }: ProductCardProps) => {
             className="w-full h-full object-contain"
           />
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="mt-4">{truncateText(data.name)}</div>
+        <div>
+          <Rating value={productRating} readOnly />
+        </div>
+        <div>{data.reviews.length} reviews</div>
+        <div className="font-semibold">{formatPrice(data.price)}</div>
       </div>
     </div>
   );
