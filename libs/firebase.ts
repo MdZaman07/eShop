6f22a591c81010052b3b10f2675a6899eaa201ff
Pulok <pulok@property.credit>
2024-03-03 10:07:40 +1100

    admin nav set up and schema update

---
+++ libs/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqQhlK_lyqTi2whGKoi3FftWZpezjPK64",
+  authDomain: "pulok-s-ecom.firebaseapp.com",
+  projectId: "pulok-s-ecom",
+  storageBucket: "pulok-s-ecom.appspot.com",
+  messagingSenderId: "125896969774",
+  appId: "1:125896969774:web:7ca6d95884a27aca5c99bf",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
